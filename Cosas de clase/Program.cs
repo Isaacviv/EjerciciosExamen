e3d375adc5e41091f33956c1ce8946c9ff686295
Isaacviv <isaacvivesartiaga@gmail.com>
2020-11-06 00:07:26 +0100

    First comit
    This is the exercise i have to do

---
+++ Cosas de clase/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cosas_de_clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pruebas.Prueba2(30);
+        }
+    }
+}
---
+++ Cosas de clase/Pruebas.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Cosas_de_clase
+{
+    class Pruebas
+    {
+        public static void Prueba(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0 ; j < n; j++)
+                {
+                    System.Console.Write("*");
+                }
+                System.Console.WriteLine();
+            }
+        }
+
+        public static void Prueba2(int n)
+        {
+            for (int r = 0; r < n; r++)
+            {
+                int nespacios = n - r - 1;
+                for (int c = 0; c <= r; c++)
+                {
+                    if (c < 1)
+                    {
+                        for (int c1 = 0; c1 < nespacios; c1++)
+                        {
+                            System.Console.Write(" ");
+                        }
+                    }
+                    System.Console.Write("*");
+
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
